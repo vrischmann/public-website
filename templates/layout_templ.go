5d4b451450b50a5711da0745ae7a6a7927d4b7b7
Vincent Rischmann <vincent@rischmann.fr>
2024-01-25 13:57:38 +0100

    ui: update generated code

--- templates/layout_templ.go
+++ templates/layout_templ.go
@@ -271,7 +271,7 @@ func Page(title string, assets Assets, body templ.Component) templ.Component {
 			templ_7745c5c3_Var16 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html lang=\"en\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
