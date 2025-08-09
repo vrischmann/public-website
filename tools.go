5366b290fdb813fada58037aa6e7ff8453384ce9
Vincent Rischmann <vincent@rischmann.fr>
2025-08-09 18:24:21 +0200

    remove obsolete tools file

--- tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-
-package tools
-
-// See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
-
-import (
-	_ "golang.org/x/tools/cmd/stringer"
-)
